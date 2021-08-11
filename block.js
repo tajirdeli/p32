46536c93810ad2a5a700585c8b3724fe8070a27f
tajirdeli <72798749+tajirdeli@users.noreply.github.com>
2021-08-11 14:39:32 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,40 @@
+
+  class Block {
+    constructor(x, y, width, height) {
+      var options = {
+          'restitution':0.8,
+         'friction':0
+        
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, this.body);
+      this.visibility=255
+    }
+    display(){
+      if(this.body.speed<3){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      stroke ('green')
+      strokeWeight(5)
+      fill(255);
+      rect(0, 0, this.width, this.height);
+      pop();
+      }
+      else{
+        World.remove(world,this.body)
+        this.visibility=this.visibility-10}
+    }
+    Score(){
+      if (this.visibility<0&&this.visibility>-6){
+        score=score+1
+      }
+    }
+  };
+            
